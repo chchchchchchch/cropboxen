f95af01e1ac13d44e947cae4456039d3337a8f83
christoph <christoph@lafkon.net>
2019-04-04 16:33:29 +0200

    getting there

--- cropboxen.php
+++ cropboxen.php
@@ -1,16 +1,88 @@
-<?php // GLOBALS
-      // -------------------------------------------------------------- //
-         $mainSrcDir  = "_";
-      // -------------------------------------------------------------- //
-      // LOAD VARIABLES
-      // -------------------------------------------------------------- //
-         $boxlist = "_/68b329/7c56127c.bxn";   
-         $svgUrl  = "https://github.sfds/adsdsdf/bla.svg";
+<?php 
+
+  // -----------------------------------------------------------------------  //
+  // GLOBALS
+  // -----------------------------------------------------------------------  //
+     $srcBasePath = "_";
+     $srcID  = "00b329"; // TODO: $_GET
+
+  // -----------------------------------------------------------------------  //
+  // LOAD/SET VARIABLES
+  // -----------------------------------------------------------------------  //
+     $versions = getVersions($srcBasePath."/".$srcID); // TODO: CHECK
+     if ( isset( $_GET['v'] )) { 
+
+      $viD = strip_tags(trim($_GET['v'])); // TODO: DIFFERENTS FORMATS/VERIFY
+
+     }
+
+     $boxList = $srcBasePath . "/" . $srcID . "/" . $viD . ".bxn";
+     $vConf   = $srcBasePath . "/" . $srcID . "/" . $viD . ".txt";
+  // -----------------------------------------------------------------------  //
+     $conf = loadConfig($srcBasePath."/".$srcID."/".$viD);
+     $layers = getLayers($srcBasePath."/".$srcID."/".$viD,$conf);
+
+     $svgWdth = $conf['W'];
+     $svgHght = $conf['H'];
+     $zeroX   = explode(':',$conf['AREA'])[0];
+     $zeroY   = explode(':',$conf['AREA'])[1];
+     $gitUrl  = $conf['GITURL'];
+     $bgColor = $conf['BGCOLOR'];     
+  // -----------------------------------------------------------------------  //
+     function getVersions($srcPath) {
+
+       $versions = array();
+       foreach (glob($srcPath."/*.txt") as $version) {
+
+                $fiD       = rtrim(substr($version,-16,12));// TODO: BETTER
+                $showDate  = "21.2.2019 10:30";
+                $versions[$fiD] = $showDate;
+       }
+
+       return $versions;
+     }
+  // -----------------------------------------------------------------------  //
+     function getLayers($vBasePath,$conf) {
+
+      $layers = array();$count = 100001;
+      foreach ( glob($vBasePath . "*.svg") as $layerFile ) {
+
+                $liD = rtrim(substr($layerFile,-10,6));
+                $layerName = $conf[$liD];
+                $visibility = rtrim(substr($layerFile,-12,1));
+                $zIndex = substr($count,-4);
+                $layers[$liD] = array('layerfile'  => $layerFile,
+                                      'layername'  => $layerName,
+                                      'zindex'     => $zIndex,
+                                      'visibility' => $visibility);
+                $count++;
+      }
+
+      return $layers;
+     }
+  // -----------------------------------------------------------------------  //
+     function loadConfig($vBasePath) {
+
+         global $gitUrl,$svgWdth,$svgHght;
+         $gitUrl  = "https://github.sfds/adsdsdf/bla.svg";
          $svgWdth = "11916";
          $svgHght = "7087";
 
+         $configFile = array_values(array_filter(file($vBasePath.".txt"),"trim"));
+    
+         $confLoad = array();
+         foreach($configFile as $line) {
+       
+            $key = trim(explode(':',$line)[0]);          
+            $val = trim(implode(":",array_slice(explode(':',$line),1)));
+            $confLoad[$key] = $val;
+
+         }
+
+         return $confLoad;
+     }
   // -----------------------------------------------------------------------  //
-     function getCropBoxes($boxlist) {
+     function loadCropBoxes($boxlist) {
  
         $list = array_values(array_filter(file($boxlist),"trim"));
  
@@ -44,7 +116,10 @@
         return $select;
      }
   // -----------------------------------------------------------------------  //
-  
+     function humanTime($timestamp) {  
+
+
+     }
   // -----------------------------------------------------------------------  //
 ?>
 <!doctype html>
@@ -59,36 +134,59 @@
  <script src="lib/js/jquery.imgareaselect.js"></script>
  <script src="lib/js/jquery.md5.js"></script>
  <script src="lib/js/panzoom.js"></script>
- <script>   var svgUrl = <?php echo '"' . $svgUrl . '"'; ?>;
+ <script>   var gitUrl = <?php echo '"' . $gitUrl . '"'; ?>;
           var svgWidth = <?php echo $svgWdth; ?>;
-             var zeroX = -1646; // srcOffsetX ???
-             var zeroY = -1617; // srcOffsetY ???
-         var svgLayers = { "5acfd7":"Layer_1",
-                           "65f439":"Layer_2",
-                           "f27510":"Layer_3",
-                           "947d72":"Layer_4" }
+             var zeroX = <?php echo $zeroX; ?>; // srcOffsetX ???
+             var zeroY = <?php echo $zeroY; ?>; // srcOffsetY ???
+         var svgLayers = { <?php $comma ="";
+                            foreach ($layers as $liD => $layerConf) { 
+                                     $layerName = $layers[$liD]['layername'];
+                                     echo $comma     . 
+                                         '"'         .
+                                          $liD       . 
+                                         '":"'       . 
+                                          $layerName . 
+                                         '"';
+                            $comma = ",\n                           ";
+                         }
+                         ?> }
  </script>
  <script src="cropboxen.js"></script>
 </head>
 <body>
-
 <div class="svg" id="svg">
 <div class="layer">
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
      width="100%" viewBox="0 0 <?php echo $svgWdth." ".$svgHght;?>" id="fillBackground">
 <rect x="0" y="0" width="<?php echo $svgWdth; ?>" height="<?php echo $svgHght; ?>" 
-      fill="#ff0000" fill-opacity="0.4" stroke="none"></rect>
+      fill="<?php echo $bgColor; ?>" fill-opacity="0.4" stroke="none"></rect>
 </svg></div>
-<div class="layer src 5acfd7" style="background-image:url('_/68b329/7c56127c_00010_5acfd7.svg');z-index:0001;"></div>
-<div class="layer src 65f439" style="background-image:url('_/68b329/7c56127c_00021_65f439.svg');z-index:0002;"></div>
-<div class="layer src f27510" style="background-image:url('_/68b329/7c56127c_00031_f27510.svg');z-index:0003;"></div>
-<div class="layer src 947d72" style="background-image:url('_/68b329/7c56127c_00041_947d72.svg');z-index:0004;"></div>
+<?php foreach ($layers as $liD => $layerConf) {
+
+               $layerFile  = $layers[$liD]['layerfile'];
+               $visibility = $layers[$liD]['visibility'];
+               if ( $visibility == 0 ) {
+                      $display = "display:none";
+               } else { $display = ""; }
+               $zIndex     = $layers[$liD]['zindex']; 
+
+               echo '<div class="layer src ' . 
+                     $liD . 
+                    '" style="background-image:url(\'' .
+                     $layerFile .
+                    '\');z-index:' . 
+                     $zIndex . 
+                    ';' .
+                     $display .
+                    '"></div>' . "\n";
+      }
+?>
 <div class="layer" id="selections">
 <svg xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink"
      width="100%" viewBox="0 0 <?php echo $svgWdth." ".$svgHght;?>"
      id="showCropBoxes">
-<?php $cropBoxes = getCropBoxes($boxlist);
+<?php $cropBoxes = loadCropBoxes($boxList);
       foreach ($cropBoxes as $id => $area) {
 
                $x = trim(explode(':',$area)[0]);
@@ -111,13 +209,40 @@
 </div>
 <div id="viewport" style="position:fixed;width:100vw;height:100vh;z-index:100000"></div>
 <div id="layercontrol">
- <input type="checkbox" id="5acfd7" class="layerSwitch" checked="checked">fdsfs <br>
- <input type="checkbox" id="65f439" class="layerSwitch" checked="checked">fdsfs <br>
- <input type="checkbox" id="f27510" class="layerSwitch" checked="checked">fdsfs <br>
- <input type="checkbox" id="947d72" class="layerSwitch" checked="checked">fdsfs <br>
+<?php foreach ($layers as $liD => $layerConf) {
+
+               $layerName = $layers[$liD]['layername'];
+               $visibility = $layers[$liD]['visibility'];
+               if ( $visibility == 0 ) {
+                      $checked = "";
+               } else { $checked = 'checked="checked"'; }
+
+                 echo ' <input type="checkbox" id="' .
+                        $liD . 
+                       '" class="layerSwitch" ' . 
+                        $checked .
+                       'autocomplete="off">' . 
+                        $layerName .
+                       '<br>' . "\n";
+      }
+?>
 </div>
 <div class="showmdsh">
 <textarea name="text" wrap="soft" id="showmdsh"></textarea>
+</div>
+<div id="versions">
+<select id="switchversion">
+<?php foreach ($versions as $fiD => $showDate) {
+      if ( $fiD == $viD ) { 
+           $selected = " selected";
+      } else { $selected = ""; }
+      echo '<option value="' . 
+            $fiD . 
+           '"' . $selected . '>' . 
+            $showDate . '</option>' . "\n";
+      }
+?>
+</select>
 </div>
 
 </body>
